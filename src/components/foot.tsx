8815becc59fe6750770f0201a7e93e3b75239437
darshee-m <darshee.m@somaiya.edu>
2024-05-23 17:31:16 -0400

    changed repo link

--- src/components/foot.tsx
+++ src/components/foot.tsx
@@ -29,7 +29,7 @@ const Foot = () => {
         &copy; {new Date().getFullYear()} Darshee Machhar <br />
       </p>
       <a
-        href='https://github.com/Darshee-m/DarsheeMachhar.github.io/'
+        href='https://github.com/Darshee-m/Portfolio'
         target='_blank'
         rel='noreferrer'
         className='mt-4 flex cursor-pointer items-center gap-1 text-[18px] font-medium text-text transition-transform duration-200 hover:-translate-y-[2px] focus:-translate-y-[2px] hover:text-primary focus:text-primary'
